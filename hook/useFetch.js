621a4f23e61cf94ec02d1165317672d97ace5e41
Bahram Al-Zuhairi <bahram.alzuhairi@hyperisland.se>
2023-09-18 20:06:28 +0200

    Fixed exposed API key

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+import { RAPID_API_KEY } from "@env";
+
 const useFetch = (endpoint, query) => {
+  const rapidApiKey = RAPID_API_KEY;
+
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -10,7 +14,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": ,
+      "X-RapidAPI-Key": rapidApiKey,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
